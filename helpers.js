56be18060b33d71eb92f7ae73ee20bafe8bda294
Peter Anglea <peter@peteranglea.com>
2016-02-17 12:48:15 -0500

    add bgcolor to rows and single columns; update link markdown helper

--- helpers.js
+++ helpers.js
@@ -2,7 +2,7 @@ Handlebars.registerHelper('mailbuilder-bju-paragraph', function(data) {
 	var returnValue = data;
 	var returnValue = returnValue.replace(/\n/g, '<br />');
 	var returnValue = returnValue.replace(/<(\/?)(script)(\s*\/?)>/gi, '&lt;$1$2$3&gt;');
-	var returnValue = returnValue.replace(/\[(.+)\]\(((http|https|mailto).+)\)/gi, '<a href="$2" style="color: #00b5ef;">$1</a>');
+	var returnValue = returnValue.replace(/\[(.+)\]\(((http|https|mailto|#).+)\)/gi, '<a href="$2" style="color: #00b5ef;">$1</a>');
 	var returnValue = returnValue.replace(/\*\*\*([^\s\*].+[^\s\*])\*\*\*/, '<b><i>$1</i></b>');
 	var returnValue = returnValue.replace(/\*\*([^\s\*].+[^\s\*])\*\*/, '<b>$1</b>');
 	var returnValue = returnValue.replace(/\*([^\s\*].+[^\s\*])\*/, '<i>$1</i>');
@@ -12,7 +12,7 @@ Handlebars.registerHelper('mailbuilder-bju-paragraph', function(data) {
 Handlebars.registerHelper('mailbuilder-bju-heading', function(data) {
 	var returnValue = data;
 	var returnValue = returnValue.replace(/<(\/?)(script)(\s*\/?)>/gi, '&lt;$1$2$3&gt;');
-	var returnValue = returnValue.replace(/\[(.+)\]\(((http|https|mailto).+)\)/gi, '<a href="$2" style="color: #00b5ef;">$1</a>');
+	var returnValue = returnValue.replace(/\[(.+)\]\(((http|https|mailto|#).+)\)/gi, '<a href="$2" style="color: #00b5ef;">$1</a>');
 	var returnValue = returnValue.replace(/\*\*\*([^\s\*].+[^\s\*])\*\*\*/, '<b><i>$1</i></b>');
 	var returnValue = returnValue.replace(/\*\*([^\s\*].+[^\s\*])\*\*/, '<b>$1</b>');
 	var returnValue = returnValue.replace(/\*([^\s\*].+[^\s\*])\*/, '<i>$1</i>');
@@ -23,7 +23,7 @@ Handlebars.registerHelper('mailbuilder-bju-list', function(data) {
 	var returnValue = data;
 	var returnValue = returnValue.replace(/^(?!\*\s)/gm, '<br />'); // line breaks that don't begin with a new list item
 	var returnValue = returnValue.replace(/<(\/?)(script)(\s*\/?)>/gi, '&lt;$1$2$3&gt;');
-	var returnValue = returnValue.replace(/\[(.+)\]\(((http|https|mailto).+)\)/gi, '<a href="$2" style="color: #00b5ef;">$1</a>');
+	var returnValue = returnValue.replace(/\[(.+)\]\(((http|https|mailto|#).+)\)/gi, '<a href="$2" style="color: #00b5ef;">$1</a>');
 	var returnValue = returnValue.replace(/\*\*\*([^\s\*].+[^\s\*])\*\*\*/, '<b><i>$1</i></b>');
 	var returnValue = returnValue.replace(/\*\*([^\s\*].+[^\s\*])\*\*/, '<b>$1</b>');
 	var returnValue = returnValue.replace(/\*([^\s\*].+[^\s\*])\*/, '<i>$1</i>');
