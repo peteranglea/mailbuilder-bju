151ef73d1bccd17fbdf754d67e47307d8ad7770b
Peter Anglea <peter@peteranglea.com>
2016-02-18 13:49:12 -0500

    updated bold/italics markdown helper

--- helpers.js
+++ helpers.js
@@ -3,9 +3,9 @@ Handlebars.registerHelper('mailbuilder-bju-paragraph', function(data) {
 	returnValue = returnValue.replace(/\n/g, '<br />');
 	returnValue = returnValue.replace(/<(\/?)(script)(\s*\/?)>/gi, '&lt;$1$2$3&gt;');
 	returnValue = returnValue.replace(/\[(.+)\]\(((http|https|mailto|#).+)\)/gi, '<a href="$2" style="color: #00b5ef;">$1</a>');
-	returnValue = returnValue.replace(/\*\*\*([^\s\*].+[^\s\*])\*\*\*/, '<b><i>$1</i></b>');
-	returnValue = returnValue.replace(/\*\*([^\s\*].+[^\s\*])\*\*/, '<b>$1</b>');
-	returnValue = returnValue.replace(/\*([^\s\*].+[^\s\*])\*/, '<i>$1</i>');
+	returnValue = returnValue.replace(/\*\*\*([^\s\*][^\*]*)\*\*\*/, '<b><i>$1</i></b>');
+	returnValue = returnValue.replace(/\*\*([^\s\*][^\*]*)\*\*/, '<b>$1</b>');
+	returnValue = returnValue.replace(/\*([^\s\*][^\*]*)\*/, '<i>$1</i>');
 	return returnValue;
 });
 
@@ -13,9 +13,9 @@ Handlebars.registerHelper('mailbuilder-bju-heading', function(data) {
 	var returnValue = data;
 	returnValue = returnValue.replace(/<(\/?)(script)(\s*\/?)>/gi, '&lt;$1$2$3&gt;');
 	returnValue = returnValue.replace(/\[(.+)\]\(((http|https|mailto|#).+)\)/gi, '<a href="$2" style="color: #00b5ef;">$1</a>');
-	returnValue = returnValue.replace(/\*\*\*([^\s\*].+[^\s\*])\*\*\*/, '<b><i>$1</i></b>');
-	returnValue = returnValue.replace(/\*\*([^\s\*].+[^\s\*])\*\*/, '<b>$1</b>');
-	returnValue = returnValue.replace(/\*([^\s\*].+[^\s\*])\*/, '<i>$1</i>');
+	returnValue = returnValue.replace(/\*\*\*([^\s\*][^\*]*)\*\*\*/, '<b><i>$1</i></b>');
+	returnValue = returnValue.replace(/\*\*([^\s\*][^\*]*)\*\*/, '<b>$1</b>');
+	returnValue = returnValue.replace(/\*([^\s\*][^\*]*)\*/, '<i>$1</i>');
 	return returnValue;
 });
 
@@ -24,9 +24,9 @@ Handlebars.registerHelper('mailbuilder-bju-list', function(data) {
 	returnValue = returnValue.replace(/^(?!\*\s)/gm, '<br />'); // line breaks that don't begin with a new list item
 	returnValue = returnValue.replace(/<(\/?)(script)(\s*\/?)>/gi, '&lt;$1$2$3&gt;');
 	returnValue = returnValue.replace(/\[(.+)\]\(((http|https|mailto|#).+)\)/gi, '<a href="$2" style="color: #00b5ef;">$1</a>');
-	returnValue = returnValue.replace(/\*\*\*([^\s\*].+[^\s\*])\*\*\*/, '<b><i>$1</i></b>');
-	returnValue = returnValue.replace(/\*\*([^\s\*].+[^\s\*])\*\*/, '<b>$1</b>');
-	returnValue = returnValue.replace(/\*([^\s\*].+[^\s\*])\*/, '<i>$1</i>');
+	returnValue = returnValue.replace(/\*\*\*([^\s\*][^\*]*)\*\*\*/, '<b><i>$1</i></b>');
+	returnValue = returnValue.replace(/\*\*([^\s\*][^\*]*)\*\*/, '<b>$1</b>');
+	returnValue = returnValue.replace(/\*([^\s\*][^\*]*)\*/, '<i>$1</i>');
 
 	returnValue = returnValue.replace(/$\n\*\s/gm, '</li>\n* ').replace(/^\*\s/gm, '<li>');
 	returnValue += "</li>"; // basic list item markdown
