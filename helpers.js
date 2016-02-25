4b20deb8a1403be2659ee799dbdab1136095326e
Peter Anglea <peter@peteranglea.com>
2016-02-25 15:46:53 -0500

    fixed bug in link markdown helper

--- helpers.js
+++ helpers.js
@@ -2,7 +2,7 @@ Handlebars.registerHelper('mailbuilder-bju-paragraph', function(data) {
 	var returnValue = data;
 	returnValue = returnValue.replace(/\n/g, '<br />');
 	returnValue = returnValue.replace(/<(\/?)(script)(\s*\/?)>/gi, '&lt;$1$2$3&gt;');
-	returnValue = returnValue.replace(/\[(.+)\]\(((http|https|mailto|#).+)\)/gi, '<a href="$2" style="color: #00b5ef;">$1</a>');
+	returnValue = returnValue.replace(/\[(.+)\]\(((http|https|mailto|#).*?)\)/gi, '<a href="$2" style="color: #00b5ef;">$1</a>');
 	returnValue = returnValue.replace(/\*\*\*([^\s\*][^\*]*)\*\*\*/g, '<b><i>$1</i></b>');
 	returnValue = returnValue.replace(/\*\*([^\s\*][^\*]*)\*\*/g, '<b>$1</b>');
 	returnValue = returnValue.replace(/\*([^\s\*][^\*]*)\*/g, '<i>$1</i>');
@@ -12,7 +12,7 @@ Handlebars.registerHelper('mailbuilder-bju-paragraph', function(data) {
 Handlebars.registerHelper('mailbuilder-bju-heading', function(data) {
 	var returnValue = data;
 	returnValue = returnValue.replace(/<(\/?)(script)(\s*\/?)>/gi, '&lt;$1$2$3&gt;');
-	returnValue = returnValue.replace(/\[(.+)\]\(((http|https|mailto|#).+)\)/gi, '<a href="$2" style="color: #00b5ef;">$1</a>');
+	returnValue = returnValue.replace(/\[(.+)\]\(((http|https|mailto|#).*?)\)/gi, '<a href="$2" style="color: #00b5ef;">$1</a>');
 	returnValue = returnValue.replace(/\*\*\*([^\s\*][^\*]*)\*\*\*/g, '<b><i>$1</i></b>');
 	returnValue = returnValue.replace(/\*\*([^\s\*][^\*]*)\*\*/g, '<b>$1</b>');
 	returnValue = returnValue.replace(/\*([^\s\*][^\*]*)\*/g, '<i>$1</i>');
@@ -23,7 +23,7 @@ Handlebars.registerHelper('mailbuilder-bju-list', function(data) {
 	var returnValue = data;
 	returnValue = returnValue.replace(/^(?!\*\s)/gm, '<br />'); // line breaks that don't begin with a new list item
 	returnValue = returnValue.replace(/<(\/?)(script)(\s*\/?)>/gi, '&lt;$1$2$3&gt;');
-	returnValue = returnValue.replace(/\[(.+)\]\(((http|https|mailto|#).+)\)/gi, '<a href="$2" style="color: #00b5ef;">$1</a>');
+	returnValue = returnValue.replace(/\[(.+)\]\(((http|https|mailto|#).*?)\)/gi, '<a href="$2" style="color: #00b5ef;">$1</a>');
 	returnValue = returnValue.replace(/\*\*\*([^\s\*][^\*]*)\*\*\*/g, '<b><i>$1</i></b>');
 	returnValue = returnValue.replace(/\*\*([^\s\*][^\*]*)\*\*/g, '<b>$1</b>');
 	returnValue = returnValue.replace(/\*([^\s\*][^\*]*)\*/g, '<i>$1</i>');
